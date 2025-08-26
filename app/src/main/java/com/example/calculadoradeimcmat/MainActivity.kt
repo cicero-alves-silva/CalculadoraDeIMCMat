1dcc81febf92557dfbb0012f426c39a37a4a7454
CICERO <cicero.alves@ifpb.edu.br>
2025-08-26 09:00:06 -0300

    Aula 25/08/2025

--- app/src/main/java/com/example/calculadoradeimcmat/MainActivity.kt
+++ app/src/main/java/com/example/calculadoradeimcmat/MainActivity.kt
@@ -1,20 +1,54 @@
 package com.example.calculadoradeimcmat
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.calculadoradeimcmat.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btOk.setOnClickListener { view ->
+            var categoria = ""
+            when(binding.rgCategorias.checkedRadioButtonId){
+                binding.rbAdultos.id -> categoria = "ADULTO"
+                binding.rbIdosos.id -> categoria = "IDOSO"
+                else ->
+                    Snackbar
+                        .make(
+                            view,
+                            "Selecione uma as opções acima antes de clicar",
+                            Snackbar.LENGTH_LONG
+                        )
+                        .setBackgroundTint(Color.RED)
+                        .setTextColor(Color.WHITE)
+                        .show()
+            }
+            if (categoria.isNotEmpty()) {
+                //ABRIR A TELA DE CÁLCULO
+            }
+        }
+
+        binding.includeAppBar.materialToolbar.setOnMenuItemClickListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.miDireitosAutorais -> {
+                    //ABRIR A TELA DE DIREITOS AUTORAIS
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
+
+
+
+
+
+
+
+
